5239ca12787fcbea8af83635e16df6e0855cc079
gusevskiy <vlgu@mail.ru>
2022-12-29 22:28:15 +0300

    bubble_sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -4,14 +4,31 @@ offset_arr = [90, 11, 40, 167, 19]
 #     print(offset_arr[i])
 def bubblesort(array):
     length = len(array)
-    for i in range(length-1):
-        for x in range(length-1-i):
-            # если соседний элемент меньше
-            if array[x+1] < array[x]:
-                # меняем их местами (можно через множ присваивание a,b=b,a)
-                temporary = array[x]
-                array[x] = array[x+1]
-                array[x+1] = temporary
+    # count = 0
+    # for i in range(length-1):
+    #     for x in range(length-1-i):
+    #         # если соседний элемент меньше
+    #         if array[x+1] < array[x]:
+    #             # меняем их местами (можно через множ присваивание a,b=b,a)
+    #             temporary = array[x]
+    #             array[x] = array[x+1]
+    #             array[x+1] = temporary
+    #         count += 1
+    # return array, count
+
+# тоже самое через while
+    i = 0
+    while i<(length-1):
+        j = 0
+        while j<(length-1)-i:
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+            j+=1
+        i+=1
     return array
 
-print(bubblesort(offset_arr))
+
+
+
+
+print(*bubblesort(offset_arr))
